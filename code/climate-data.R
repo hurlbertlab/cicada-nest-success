3963c640f1c3693a273fe4b424c866d017b5627c
inieri@unc.edu <inieri@AD.UNC.EDU>
2025-03-18 11:00:59 -0400

    working on downloading daymet data

--- code/climate-data.R
+++ code/climate-data.R
@@ -5,45 +5,54 @@ library(tidyr)
 
 
 # Define a set of latitude and longitude points within the eastern United States
-points <- data.frame(
-  site = c("point1", "point2", "point3", "point4"), 
-  lat = c(35.0, 40.0, 30.0, 45.0),                 
-  lon = c(-80.0, -75.0, -85.0, -70.0)              
-)
+#points <- data.frame(
+ # site = c("point1", "point2", "point3", "point4"), 
+ #lat = c(35.0, 40.0, 30.0, 45.0),                 
+  #lon = c(-80.0, -75.0, -85.0, -70.0)              
+#)
 
-start_year <- 2007
-end_year <- 2024
+#start_year <- 2007
+#end_year <- 2024
 
-data_list <- list()
+#data_list <- list()
 
 # Loop through each point and download data
-for (i in 1:nrow(points)) {
+#for (i in 1:nrow(points)) {
   
   
-  daymet_data <- download_daymet(
-    site = points$site[i],
-    lat = poinst$lat[i],
-    lon = points$lon[i],
-    start = start_year,
-    end = end_year,
-    internal = TRUE
-  )
+  #daymet_data <- download_daymet(
+    #site = points$site[i],
+    #lat = poinst$lat[i],
+    #lon = points$lon[i],
+    #start = start_year,
+    #end = end_year,
+    #internal = TRUE
+ # )
   
-  data_list[[i]] <- as.data.frame(daymet_data$data)
-}
+ # data_list[[i]] <- as.data.frame(daymet_data$data)
+#}
+
+# Create CSV in The format of: site name, latitude, longitude, where each site is a nestbox
+nest_summaries <- read.csv("data/filtered_summaries.csv") %>%
+  filter(startsWith(Subnational.Code, "US-")) %>%
+  drop_na(Young.Total) %>%
+  select(Location.ID,Latitude,Longitude)
+
+write.csv(nest_summaries,"data/nestcoordinates.csv",row.names = FALSE)
+
 
 # Get raw daymet
-daymet_data <- download_daymet_batch(file_location = 'my_sites.csv',
+daymet_data <- download_daymet_batch(file_location = "data/nestcoordinates.csv",
                       start = 1980,
                       end = 2010,
                       internal = TRUE) %>%
-  mutate(tmean = ...) %>%
+  mutate(tmean = ((tmin..deg.c. + tmax..deg.c.) / 2)) %>%
   group_by(site, year) %>%
   summarize(summerTemp = mean(tmean[yday %in% 153:213], na.rm = TRUE),
-            summerPrecip = sum(prcp.mm.day.[yday %in% 153:213], na.rm = TRUE))
+            summerPrecip = sum(prcp.mm.day.[yday %in% 153:213], na.rm = TRUE)) 
 
 write.csv(daymet_data)
-
+ 
 #
 historical_climate <- daymet_data %>% 
   group_by(site) %>%
